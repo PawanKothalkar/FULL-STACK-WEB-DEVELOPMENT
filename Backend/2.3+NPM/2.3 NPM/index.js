075d75855a918642bbf76f604819af8cb9c2c18a
PawanKothalkar <100119712+PawanKothalkar@users.noreply.github.com>
2025-04-26 09:25:05 +0530

    commit

---
+++ Backend/2.3+NPM/2.3 NPM/index.js
@@ -0,0 +1,12 @@
+// var generateName = require("sillyname");
+// var sillyName = generateName();
+
+
+
+import superheroes from 'superheroes';
+
+superheroes;
+//=> ['3-D Man', 'A-Bomb', …]
+import {randomSuperhero} from 'superheroes';
+var superhero=randomSuperhero();
+console.log(`my superhero name is ${superhero}`);
